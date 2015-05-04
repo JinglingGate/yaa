ca611bf46c9d7c335b6f2bf30b8e69985a8e362e
JB <info@thegraphicsgarden.org>
2015-05-04 12:44:15 -0700

    First Dummy Methods
    Created some CRUD methods to get going with dummy results. Need to make
    sure this is okay to take parameters…

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,6 +7,18 @@ class ApiController < ApplicationController
     when "test"
       test
     end
+    when "userAddPin"
+      userAddPin
+    end
+    when "getInstancesOf"
+      getInstancesOf
+    end
+    when "getUserPinsOfType"
+      getUserPinsOfType
+    end
+    when "userDeletePin"
+      userDeletePin
+    end
   end
 
   def test
@@ -19,6 +31,69 @@ class ApiController < ApplicationController
           keyTwo: "valTwo"
         }
       }
+  end
+
+  #Create 
+  def userAddPin(userId, time, aggressionType, aggressionLat, aggressionLong)
+    render json: {
+      userId: 1,
+      time: "2015-03-28T23:14:04Z",
+      aggressionType: "Sexism",
+      coordinate: [37.3492, -121.9381]
+    }
+  end
+
+  #Read 
+  def getInstancesOf(aggressionType, withinRadius, withinDays)
+    render json: {
+      aggression: "Sexism",
+      instances: [ 
+        {
+          userId: 1,
+          time: "2015-04-1T12:04:00Z",
+          coordinate: [37.3492, -121.9876]
+        },{
+          userId: 2,
+          time: "2015-04-2T12:04:00Z",
+          coordinate: [37.3492, -121.8765]
+        },{
+          userId: 3,
+          time: "2015-04-3T12:04:00Z",
+          coordinate: [37.3492, -121.7654]
+        }  
+      ]
+    }
+  end 
+  def getUserPinsOfType(aggressionType, userId, withinRadius, withinDays)
+    render json: {
+      userId: 1,
+      aggression: "Racism",
+      instances: [ 
+        {
+          userId: 1,
+          time: "2015-04-1T12:04:00Z",
+          coordinate: [37.3492, -121.9876]
+        },{
+          time: "2015-04-2T12:04:00Z",
+          coordinate: [37.3492, -121.8765]
+        },{
+          time: "2015-04-3T12:04:00Z",
+          coordinate: [37.3492, -121.7654]
+        }  
+      ]
+    }
+  end
+
+  #Update?
+
+  #Delete? 
+  def userDeletePin(userId, time, aggressionType, aggressionLat, aggressionLong)
+    render json: {
+      userId: 1,
+      time: "2015-03-28T23:14:04Z",
+      aggression: "Sexism",
+      coordinate: [37.3492, -121.9381]
+    }
   end
 end
 
