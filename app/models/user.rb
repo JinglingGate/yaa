90880f5e08f001e196da677209a9f52d9ce2d567
cbestiva <cbestiva@gmail.com>
2015-05-04 18:22:38 -0700

    added and passing user validation tests, added and passing capitalized first and last name test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email, :username, :password, :password_confirmation
+  validates_uniqueness_of :email
+  validates :username, length: {minimum: 7}
+  has_secure_password  
+  validates :password, length: {minimum: 7}
+  before_save :capitalize_name
+
+  def capitalize_name
+    self.first_name = self.first_name.capitalize
+    self.last_name = self.last_name.capitalize
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#RSpec.describe User, type: :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+describe User do
+  describe 'validations' do
+    subject {User.new(first_name: 'charlene', last_name: 'estiva', email: 'charlene@test.com', username: 'cestiva', password: 'test123', password_confirmation: 'test123')}
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:username)}
+    it {should validate_length_of(:username).is_at_least(7)}
+    it {should have_secure_password}
+    it {should validate_length_of(:password).is_at_least(7)}
+  end
+
+  it 'has a capitalized first and last name' do 
+    user = User.new(first_name: 'charlene', last_name: 'estiva', email: 'charlene@test.com', username: 'cestiva', password: 'test123', password_confirmation: 'test123')
+    user.save
+    expect(user.first_name).to eq('Charlene')
+    expect(user.last_name).to eq('Estiva')
+  end
 end
