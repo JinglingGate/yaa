93b19a322b74d11c674ee6dbb21f68ae47794118
cbestiva <cbestiva@gmail.com>
2015-05-04 21:38:24 -0700

    generated users controller, added signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :email, :username, :password, :password_confirmation
+  validates_presence_of :first_name, :last_name, :email, :username
   validates_uniqueness_of :email
   validates :username, length: {minimum: 7}
   has_secure_password  
   validates :password, length: {minimum: 7}
+  validates_presence_of :password_confirmation
   before_save :capitalize_name
 
   def capitalize_name
     self.first_name = self.first_name.capitalize
     self.last_name = self.last_name.capitalize
   end
-  
-  private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
-  end
-end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'api' => 'api#delegate'
   post 'api' => 'api#delegate'
+  root "users#index"
+  resources :users, except: :new
+  get '/signup', to: 'users#new'
+
 end
